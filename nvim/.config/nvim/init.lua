a4c5a0c28a4c2efa0e4adee3e6cf4e0c4494f411
aperezmarquez <aperezmarquez.business@gmail.com>
2026-01-19 18:37:29 +0100

    Fix: Removed every downgraded version or incompatible with NixOS

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -16,7 +16,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-ft = {"quarto", "markdown"}
 vim.g.codeium_enabled = false
 
 require("lazy").setup("plugins")
